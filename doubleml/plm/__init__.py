84b147001a577a9e8e3227f7b180c0ec90c5bdc8
Julian Diefenbacher <93731825+JulianDiefenbacher@users.noreply.github.com>
2025-11-30 15:12:58 +0100

    add basic plpr tests

--- doubleml/plm/__init__.py
+++ doubleml/plm/__init__.py
@@ -4,11 +4,7 @@ The :mod:`doubleml.plm` module implements double machine learning estimates base
 
 from .lplr import DoubleMLLPLR
 from .pliv import DoubleMLPLIV
-from .plr import DoubleMLPLR
 from .plpr import DoubleMLPLPR
+from .plr import DoubleMLPLR
 
-__all__ = [
-    "DoubleMLPLR",
-    "DoubleMLPLIV",
-    "DoubleMLPLPR",
-]
+__all__ = ["DoubleMLPLR", "DoubleMLPLIV", "DoubleMLLPLR", "DoubleMLPLPR"]
