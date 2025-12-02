4d38d2400d3dda6c83f29593fc997c76911be21f
Pedro Pires <pedro.fangueiro.pires@gmail.com>
2025-12-02 11:14:54 +0100

    refactor: join mediation classes into one
    This commit replaces the classes estimating the "potential"/"counterfactual"
    scores with a single class performing all the functionalities.

--- doubleml/med/__init__.py
+++ doubleml/med/__init__.py
@@ -1,8 +1,9 @@
-from .med import DoubleMLMEDP, DoubleMLMEDC
+from .med import DoubleMLMEDC, DoubleMLMediation, DoubleMLMEDP
 from .meds import DoubleMLMEDS
 
 __all__ = [
     "DoubleMLMEDP",
     "DoubleMLMEDC",
+    "DoubleMLMediation",
     "DoubleMLMEDS",
 ]
