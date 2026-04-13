a09ce21ff46984e1ce2fab7c679152eb258519e5
Pedro Pires <pedro.fangueiro.pires@gmail.com>
2026-04-13 19:11:55 +0200

    Refactor: Replaced possible score value ``'MED'`` with ``'efficient-alt'``. This is because ``'efficient-alt'`` is more in-line with the name given to the score function in the paper this implementation is based on (Farbmacher, Helmut; Huber, Martin; Lafférs, Lukáš; Langen, Henrika; Spindler, Martin. "Causal mediation analysis with double machine learning").
    Removed parameter 'score-function' since it's not usefull.

--- doubleml/med/tests/test_meds_external_predictions.py
+++ doubleml/med/tests/test_meds_external_predictions.py
@@ -19,7 +19,7 @@ def meds_kwargs(dml_data, learners, double_sample_splitting):
         "n_folds": 5,
         "n_rep": 3,
         "n_folds_inner": 5,
-        "score": "MED",
+        "score": "efficient-alt",
         "normalize_ipw": False,
         "trimming_threshold": 1e-2,
         "draw_sample_splitting": True,
